ca0b442ac0f38ce1c4eed380e8bda2327d2bd419
Nicole Hilden <nicole.hilden@gmail.com>
2018-09-22 18:06:19 -0400

    hw4a code

---
+++ githubapi.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+def get_repos(githubID):
+
+    repos = requests.get("https://api.github.com/users/" +githubID+ "/repos")
+    retrieved = json.loads(repos.text)
+
+    repo_list = []
+    
+    for i in retrieved:
+        try: repo_list += [i.get('name')]
+        except:
+            print("Cannot get repositories for "+githubID)
+            return []
+
+    return repo_list
+
+def get_commits(githubID, repo_name):
+
+    commits = requests.get("https://api.github.com/repos/" +githubID+ "/" +repo_name+ "/commits")
+    retrieved = json.loads(commits.text)
+
+    return len(retrieved)
+
+def main():
+    githubID = input("What is your GitHub username? ")
+    
+    repositories = get_repos(githubID)
+
+    for repo in repositories:
+        print("Repo: " +repo+ " Number of commits: " +str(get_commits(githubID, repo)))
+    
+
+
+if __name__ == "__main__":
+    main()
